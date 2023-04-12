41a8050490916e7686a7bd9c1de74f9e1cfef737
DTRD (Dimitris Dallas) <DTRD@novonordisk.com>
2023-04-12 14:31:59 +0200

    configure and installation template

---
+++ minikube-argocd.sh
@@ -0,0 +1,50 @@
+####################
+# Create a Cluster #
+####################
+
+minikube start
+
+#############################
+# Deploy Ingress Controller #
+#############################
+
+minikube addons enable ingress
+
+kubectl --namespace ingress-nginx wait \
+    --for=condition=ready pod \
+    --selector=app.kubernetes.io/component=controller \
+    --timeout=120s
+
+export INGRESS_HOST=$(minikube ip)
+
+###################
+# Install Argo CD #
+###################
+
+helm repo add argo https://argoproj.github.io/argo-helm
+
+helm upgrade --install argocd argo/argo-cd \
+    --namespace argocd --create-namespace \
+    --set server.ingress.hosts="{argocd.$INGRESS_HOST.nip.io}" \
+    --values argo/argocd-values.yaml --wait
+
+export PASS=$(kubectl --namespace argocd \
+    get secret argocd-initial-admin-secret \
+    --output jsonpath="{.data.password}" | base64 -d)
+
+argocd login --insecure --username admin --password $PASS \
+    --grpc-web argocd.$INGRESS_HOST.nip.io
+
+echo $PASS
+
+argocd account update-password
+
+open http://argocd.$INGRESS_HOST.nip.io
+
+cd ..
+
+#######################
+# Destroy The Cluster #
+#######################
+
+minikube delete
